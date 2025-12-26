54c7427d6766b5e6f0434e6d73e86f92447daf45
khaledxyz <khaledhalloua@outlook.com>
2025-12-26 11:55:44 +0100

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import { AsteriskIcon } from "@phosphor-icons/react";
 
 export function Logo() {
   return (
-    <div>
+    <div className="flex items-center gap-1">
       <AsteriskIcon />
       <span>{import.meta.env.VITE_APP_NAME || "keepstash"}</span>
     </div>
