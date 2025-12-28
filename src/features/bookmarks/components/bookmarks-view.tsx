c974ff7d22e5d5f3eb2e7381c3c3392c472a3cff
khaledxyz <khaledhalloua@outlook.com>
2025-12-28 14:18:25 +0100

    feat: create empty state for bookmarks

--- src/features/bookmarks/components/bookmarks-view.tsx
+++ src/features/bookmarks/components/bookmarks-view.tsx
@@ -1,14 +1,26 @@
 import { Suspense, use } from "react";
 
-import type { Bookmark } from "../api";
+import { EmptyIcon } from "@phosphor-icons/react";
+
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+
+import type { Bookmark } from "@/features/bookmarks/api";
+import { BookmarkDialog } from "@/features/bookmarks/components/bookmark-dialog";
 import {
   BookmarksGrid,
   BookmarksGridSkeleton,
-} from "../layouts/bookmarks-grid";
+} from "@/features/bookmarks/layouts/bookmarks-grid";
 import {
   BookmarksList,
   BookmarksListSkeleton,
-} from "../layouts/bookmarks-list";
+} from "@/features/bookmarks/layouts/bookmarks-list";
 
 interface Props {
   bookmarksPromise: Promise<Bookmark[]>;
@@ -31,6 +43,27 @@ export function BookmarksView({ bookmarksPromise, viewMode }: Props) {
 
 function BookmarksViewContent({ bookmarksPromise, viewMode }: Props) {
   const bookmarks = use(bookmarksPromise);
+
+  if (!bookmarks || bookmarks.length === 0) {
+    return (
+      <Empty className="border">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <EmptyIcon />
+          </EmptyMedia>
+          <EmptyTitle>No bookmarks yet</EmptyTitle>
+        </EmptyHeader>
+        <EmptyDescription>
+          You haven't added any bookmarks. <br />
+          Start saving your favorite links to see them here.
+        </EmptyDescription>
+
+        <EmptyContent>
+          <BookmarkDialog />
+        </EmptyContent>
+      </Empty>
+    );
+  }
 
   return viewMode === "grid" ? (
     <BookmarksGrid bookmarks={bookmarks} />
