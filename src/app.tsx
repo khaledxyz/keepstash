fbeb45ef6e08ca0ca541975ad7fbf01c35e22d93
khaledxyz <khaledhalloua@outlook.com>
2025-12-28 14:07:57 +0100

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,31 @@
 import { NuqsAdapter } from "nuqs/adapters/react-router/v7";
 import { Route, Routes } from "react-router";
 
-import HomePage from "./routes";
+import { EmptyLayout } from "./layouts/empty";
+import { MainLayout } from "./layouts/main";
+import { Providers } from "./providers";
 import DashboardPage from "./routes/dashboard";
+import HomePage from "./routes/home";
+import { LoginPage } from "./routes/login";
+import { RegisterPage } from "./routes/register";
 
 export default function App() {
   return (
     <NuqsAdapter>
-      <Routes>
-        <Route element={<HomePage />} index />
-        <Route element={<DashboardPage />} path="dashboard" />
-      </Routes>
+      <Providers>
+        <Routes>
+          <Route element={<HomePage />} index />
+
+          <Route element={<MainLayout />}>
+            <Route element={<DashboardPage />} path="dashboard" />
+          </Route>
+
+          <Route element={<EmptyLayout />}>
+            <Route element={<RegisterPage />} path="register" />
+            <Route element={<LoginPage />} path="login" />
+          </Route>
+        </Routes>
+      </Providers>
     </NuqsAdapter>
   );
 }
