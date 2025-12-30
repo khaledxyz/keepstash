fb93e3c2fdcd588dca473875714e1f82ec4291a6
khaledxyz <khaledhalloua@outlook.com>
2025-12-30 15:17:46 +0100

    feat: setup rate limiter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard } from "@nestjs/throttler";
+
+import { RateLimiterModule } from "infra/rate-limiter/rate-limiter.module";
 
 @Module({
-  imports: [],
+  imports: [
+    // INFRA
+    RateLimiterModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
