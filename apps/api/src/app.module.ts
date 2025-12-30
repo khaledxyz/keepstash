9927bb883008579339036d1fdda00a2174c585a0
khaledxyz <khaledhalloua@outlook.com>
2025-12-30 16:55:27 +0100

    feat: create database module with drizzle

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
 import { ThrottlerGuard } from "@nestjs/throttler";
 
+import { DatabaseModule } from "infra/database/database.module";
 import { HealthModule } from "infra/health/health.module";
 import { MetricsModule } from "infra/metrics/metrics.module";
 import { RateLimiterModule } from "infra/rate-limiter/rate-limiter.module";
@@ -14,6 +15,7 @@ import { RateLimiterModule } from "infra/rate-limiter/rate-limiter.module";
     MetricsModule,
     RateLimiterModule,
     HealthModule,
+    DatabaseModule,
   ],
   controllers: [],
   providers: [
