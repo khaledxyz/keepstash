4942cbc833807a52a9eba401550aab3fdbacb551
khaledxyz <khaledhalloua@outlook.com>
2026-01-02 11:48:26 +0100

    feat(layouts): implement ProtectedLayout with session check and loading spinner

--- apps/web/src/layouts/protected/index.tsx
+++ apps/web/src/layouts/protected/index.tsx
@@ -1,3 +1,24 @@
+import { Navigate, Outlet, useLocation } from "react-router";
+
+import { authClient } from "@/lib/auth-client";
+
+import { Spinner } from "@/components/ui/spinner";
+
 export function ProtectedLayout() {
-  return null;
+  const { data: session, isPending } = authClient.useSession();
+  const location = useLocation();
+
+  if (isPending) {
+    return (
+      <div className="absolute top-0 left-0 grid h-screen w-screen place-items-center bg-background">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (!session) {
+    return <Navigate replace state={{ from: location }} to="/login" />;
+  }
+
+  return <Outlet />;
 }
